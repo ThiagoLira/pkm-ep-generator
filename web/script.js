6fddb7ffd274594026dfa24aa4dc7e6ef9e561cb
Thiago Lira <thlira15@gmail.com>
2019-10-21 19:12:27 -0300

    new ip

--- web/script.js
+++ web/script.js
@@ -22,7 +22,7 @@ function callGeneratorAjax(){
     displayLoader(loaders,"inline-block")
     $.ajax({
         method: 'GET',
-        url: 'http://ec2-18-228-30-26.sa-east-1.compute.amazonaws.com/?sample=' + prefix,
+        url: 'http://api.thiagolira.com.br:443/?sample=' + prefix,
         // url: 'http://localhost:5000/?sample=' + prefix,  
         dataType: 'jsonp', //change the datatype to 'jsonp' works in most cases
         success: (res) => {
