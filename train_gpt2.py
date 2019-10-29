f1b2f3493cc680d5378f5e693a9c2707e6a1ad55
Thiago Lira <thlira15@gmail.com>
2019-10-29 16:24:47 -0300

    train more

--- train_gpt2.py
+++ train_gpt2.py
@@ -7,6 +7,6 @@ sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
               'data/pokeCorpusBulba/train.txt',
               model_name=model_name,
-              steps=1000)   # steps is max number of training steps
+              steps=1500)   # steps is max number of training steps
 
 gpt2.generate(sess)
